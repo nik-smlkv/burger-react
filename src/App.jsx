484449809866c86862d958e1f825a66c7e622232
nik-smlkv <nsamolev@mail.ru>
2023-02-27 18:27:08 +0300

    Получаем категории с сервера

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import { useState } from 'react'
 import { Footer } from './components/Footer/Footer.jsx';
 import { Header } from './components/Header/Header.jsx';
 import { Navigation } from './components/Navigation/Navigation.jsx';
-import {Catalog} from './components/Catalog/Catalog';
-export const  App = () => {
-  return (
-   <>
-      <Header/>
-      <main>
-      <Navigation/>
-      <Catalog/>
-      </main>
-      <Footer/>
-   </>
-  )
+import { Catalog } from './components/Catalog/Catalog';
+import { store } from './store/index.js';
+import { Provider } from 'react-redux';
+
+
+export const App = () => {
+   return (
+      <Provider store={store}>
+         <Header />
+         <main>
+            <Navigation />
+            <Catalog />
+         </main>
+         <Footer />
+      </Provider>
+   )
 }
 
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,38 +1,35 @@
 import style from './Navigation.module.css';
 import { Container } from '../Container/Container';
 import classNames from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
+import { categoryRequestAsync, changeCategory } from '../../store/category/categorySlice';
+import { useEffect } from 'react';
 export const Navigation = () => {
+
+   const { category, activeCategory } = useSelector((state) => state.category);
+   const dispatch = useDispatch();
+
+   useEffect(() =>{
+      dispatch(categoryRequestAsync ());
+   }, [])
+
    return (
       <nav className={style.navigation}>
          <Container className={style.navigation__container}>
             <ul className={style.navigation__list}>
-               <li className={style.navigation__item}>
-                  <button className={classNames(style.navigation__button, style.navigation__button_burger, style.navigation__button_active)}>Бургеры</button>
-               </li>
-               <li className={style.navigation__item}>
-                  <button className={classNames(style.navigation__button,style.navigation__button_snack)}>Закуски</button>
-               </li>
-               <li className={style.navigation__item}>
-                  <button className={classNames(style.navigation__button, style.navigation__button_hotdog)}>Хот-доги</button>
-               </li>
-               <li className={style.navigation__item}>
-                  <button className={classNames(style.navigation__button, style.navigation__button_combo)}>Комбо</button>
-               </li>
-               <li className={style.navigation__item}>
-                  <button className={classNames(style.navigation__button, style.navigation__button_shawarma)}>Шаурма</button>
-               </li>
-               <li className={style.navigation__item}>
-                  <button className={classNames(style.navigation__button, style.navigation__button_pizza)}>Пицца</button>
-               </li>
-               <li className={style.navigation__item}>
-                  <button className={classNames(style.navigation__button, style.navigation__button_wok)}>Вок</button>
-               </li>
-               <li className={style.navigation__item}>
-                  <button className={classNames(style.navigation__button, style.navigation__button_dessert)}>Десерты</button>
-               </li>
-               <li className={style.navigation__item}>
-                  <button className={classNames(style.navigation__button, style.navigation__button_sauce)}>Соусы</button>
-               </li>
+               {category.map((item, i) =>
+                  <li className={style.navigation__item} key={item.title}>
+                     <button className={classNames(style.navigation__button,
+                        activeCategory === i ? style.navigation__button_active : '')}
+                        style={{ backgroundImage: `url(${item.image})`}}
+                        onClick={() => {
+                           dispatch(changeCategory({ indexCategory: i }))
+                        }}
+                     >
+                        {item.rus}
+                     </button>
+                  </li>
+               )}
             </ul>
          </Container>
       </nav>
